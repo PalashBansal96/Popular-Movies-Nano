0e00e29804fc82559ad18d9637e4ba83a3066c95
Palash Bansal <PalashBansal96@gmail.com>
2016-03-23 00:36:12 +0530

    Movies Populated in RecyclerView

--- app/src/main/java/com/palashbansal/popularmoviesnano/MovieListActivity.java
+++ app/src/main/java/com/palashbansal/popularmoviesnano/MovieListActivity.java
@@ -3,21 +3,25 @@ package com.palashbansal.popularmoviesnano;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 
-import com.palashbansal.popularmoviesnano.dummy.DummyContent;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.palashbansal.popularmoviesnano.helpers.DBConnector;
+import com.palashbansal.popularmoviesnano.helpers.MovieItem;
+import com.squareup.picasso.Picasso;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,6 +39,11 @@ public class MovieListActivity extends AppCompatActivity {
 	 * device.
 	 */
 	private boolean mTwoPane;
+	private List<MovieItem> movies = new ArrayList<>();
+	private DBConnector.SortOrder order = DBConnector.SortOrder.POPULAR;
+	private RecyclerView recyclerView;
+	private SimpleItemRecyclerViewAdapter recyclerViewAdapter;
+	private GridLayoutManager gridLayoutManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,18 +54,23 @@ public class MovieListActivity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 		toolbar.setTitle(getTitle());
 
+		Picasso.with(getApplicationContext()).setIndicatorsEnabled(true);  //TODO: Remove in Production
+
 		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				if(order == DBConnector.SortOrder.POPULAR) {
+					order = DBConnector.SortOrder.TOP_RATED;
+				}else {
+					order = DBConnector.SortOrder.POPULAR;
+				}
+				int temp_size = movies.size();
+				movies.clear();
+				recyclerViewAdapter.notifyItemRangeRemoved(0, temp_size);
+				refreshMovieList();
 			}
 		});
-
-		View recyclerView = findViewById(R.id.movie_list);
-		assert recyclerView != null;
-		setupRecyclerView((RecyclerView) recyclerView);
 
 		if (findViewById(R.id.movie_detail_container) != null) {
 			// The detail container view will be present only in the
@@ -65,18 +79,46 @@ public class MovieListActivity extends AppCompatActivity {
 			// activity should be in two-pane mode.
 			mTwoPane = true;
 		}
+
+		if(mTwoPane) {
+			gridLayoutManager = new GridLayoutManager(this, 3);
+		}else{
+			gridLayoutManager = new GridLayoutManager(this, 2);
+		}
+		recyclerView = (RecyclerView) findViewById(R.id.movie_list);
+		recyclerViewAdapter = new SimpleItemRecyclerViewAdapter(movies);
+		setupRecyclerView(recyclerViewAdapter);
+
+		refreshMovieList();
+
 	}
 
-	private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-		recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+	private void refreshMovieList() {
+		DBConnector.discover(order, this,
+				new Response.Listener<JSONObject>() {
+					@Override
+					public void onResponse(JSONObject response) {
+						DBConnector.generateMovieObjects(response, movies, recyclerViewAdapter);
+					}
+				}, new Response.ErrorListener() {
+					@Override
+					public void onErrorResponse(VolleyError error) {
+
+					}
+				});
+	}
+
+	private void setupRecyclerView(SimpleItemRecyclerViewAdapter recyclerViewAdapter) {
+		recyclerView.setLayoutManager(gridLayoutManager);
+		recyclerView.setAdapter(recyclerViewAdapter);
 	}
 
 	public class SimpleItemRecyclerViewAdapter
 			extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-		private final List<DummyContent.DummyItem> mValues;
+		private final List<MovieItem> mValues;
 
-		public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+		public SimpleItemRecyclerViewAdapter(List<MovieItem> items) {
 			mValues = items;
 		}
 
@@ -90,15 +132,14 @@ public class MovieListActivity extends AppCompatActivity {
 		@Override
 		public void onBindViewHolder(final ViewHolder holder, int position) {
 			holder.mItem = mValues.get(position);
-			holder.mIdView.setText(mValues.get(position).id);
-			holder.mContentView.setText(mValues.get(position).content);
+			Picasso.with(getApplicationContext()).load(holder.mItem.getImageURL()).into(holder.mImageView);
 
 			holder.mView.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					if (mTwoPane) {
 						Bundle arguments = new Bundle();
-						arguments.putString(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+						arguments.putString(MovieDetailFragment.ARG_ITEM_ID, String.valueOf(holder.mItem.getId()));
 						MovieDetailFragment fragment = new MovieDetailFragment();
 						fragment.setArguments(arguments);
 						getSupportFragmentManager().beginTransaction()
@@ -107,7 +148,7 @@ public class MovieListActivity extends AppCompatActivity {
 					} else {
 						Context context = v.getContext();
 						Intent intent = new Intent(context, MovieDetailActivity.class);
-						intent.putExtra(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+						intent.putExtra(MovieDetailFragment.ARG_ITEM_ID, holder.mItem.getId());
 
 						context.startActivity(intent);
 					}
@@ -122,20 +163,18 @@ public class MovieListActivity extends AppCompatActivity {
 
 		public class ViewHolder extends RecyclerView.ViewHolder {
 			public final View mView;
-			public final TextView mIdView;
-			public final TextView mContentView;
-			public DummyContent.DummyItem mItem;
+			public final ImageView mImageView;
+			public MovieItem mItem;
 
 			public ViewHolder(View view) {
 				super(view);
 				mView = view;
-				mIdView = (TextView) view.findViewById(R.id.id);
-				mContentView = (TextView) view.findViewById(R.id.content);
+				mImageView = (ImageView) view.findViewById(R.id.content);
 			}
 
 			@Override
 			public String toString() {
-				return super.toString() + " '" + mContentView.getText() + "'";
+				return super.toString() + " '" + mItem.getTitle() + "'";
 			}
 		}
 	}
